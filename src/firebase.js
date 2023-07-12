276944c58ca4e6f15c26bf9eac492d750c54a57e
Suhani Kashyap <suhani.kashyap@mail.mcgill.ca>
2023-07-12 12:47:49 -0400

    added firebase config

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB5hHjcuXMTDLmzpp2RGJQ6tugo1Eay9-4",
+    authDomain: "netflix-clone-da1be.firebaseapp.com",
+    projectId: "netflix-clone-da1be",
+    storageBucket: "netflix-clone-da1be.appspot.com",
+    messagingSenderId: "453933513215",
+    appId: "1:453933513215:web:bb707719cb7d21d3d94fca"
+  };
