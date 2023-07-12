9ce841a242e98a562e2151a9c59337a93e77ba04
Suhani Kashyap <suhani.kashyap@mail.mcgill.ca>
2023-07-12 16:52:55 -0400

    setting up api requests for images

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -2,10 +2,30 @@ import React from 'react';
 import './Banner.css';
 
 function Banner() {
+
+  function truncate(string, n){
+    return string?.length > n ? string.substr(0, n-1) + "..." : string;
+  };
+   
   return (
-    <div className='banner'>
-        
-    </div>
+    <header className='banner' style={{
+      backgroundSize: "cover",
+      backgroundImage: `url("https://decider.com/wp-content/uploads/2018/05/netflix-logo.png?w=646&h=431&crop=1")`,
+      backgroundPosition: 'center center'
+    }}>
+      <div className='banner__contents'>
+        <h1 className='banner__title'>Movie Name</h1>
+        <div className='banner__buttons'>
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <h1 className='banner__description'>This is a test description
+          {truncate(`This is a test description`, 150)}
+        </h1>
+      </div>
+
+      <div className='banner--fadeBottom' />
+    </header>
   )
 }
 
