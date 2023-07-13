3cd7ac1d66d7c9990cbac828221f489e482c03ba
Suhani Kashyap <suhani.kashyap@mail.mcgill.ca>
2023-07-13 11:58:33 -0400

    completed banner background setup with tmdb api

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -30,17 +30,19 @@ function Banner() {
   return (
     <header className='banner' style={{
       backgroundSize: "cover",
-      backgroundImage: `url("https://decider.com/wp-content/uploads/2018/05/netflix-logo.png?w=646&h=431&crop=1")`,
+      backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
       backgroundPosition: 'center center'
     }}>
       <div className='banner__contents'>
-        <h1 className='banner__title'>Movie Name</h1>
+        <h1 className='banner__title'>
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className='banner__buttons'>
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
-        <h1 className='banner__description'>This is a test description
-          {truncate(`This is a test description`, 150)}
+        <h1 className='banner__description'>
+          {truncate(`${movie?.overview}`, 150)}
         </h1>
       </div>
 
